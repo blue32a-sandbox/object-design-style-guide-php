ab8163318716c46474870cc8ae56b04c82776fcd
blue32a <blue32a.jp@gmail.com>
2024-07-28 02:34:00 +0000

    ４.７　イミュータブルオブジェクトではモディファイアメソッドは宣言的な名前にする

---
+++ src/chapter04/4_15_moveLeft_instead_of_toTheLeft.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * 例４－１５　toTheLeft()の代わりにmoveLeft()という名前
+ */
+
+final class Position
+{
+    // ...
+
+    public function moveLeft(int $x): Position
+    {
+        // ...
+    }
+}
---
+++ src/chapter04/4_16_toTheLeft_is_a_more_suitable_name.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * 例４－１６　toTheLeft()はより適切な名前
+ */
+
+final class Position
+{
+    // ...
+
+    public function toTheLeft(int $x): Position
+    {
+        // ...
+    }
+}
