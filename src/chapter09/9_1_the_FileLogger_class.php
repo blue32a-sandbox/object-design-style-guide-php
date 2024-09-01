8895ad90aad3098462dbea9b824d4b02f022db4c
blue32a <blue32a.jp@gmail.com>
2024-09-01 00:51:32 +0000

    ９.１　振る舞いを設定可能にするためのコンストラクタ引数を導入する

---
+++ src/chapter09/9_1_the_FileLogger_class.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * 例９－１　FileLoggerクラス
+ */
+
+final class FileLogger
+{
+    public function log($message): void
+    {
+        file_put_contents(
+            '/var/log/app.log',
+            $message,
+            FILE_APPEND
+        );
+    }
+}
---
+++ src/chapter09/9_2_use_a_constructor_argument_to_configure_the_FileLogger.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * 例９－２　コンストラクタ引数でFileLoggerを設定する
+ */
+
+final class FileLogger
+{
+    private string $filePath;
+
+    public function __construct(string $filePath)
+    {
+        $this->filePath = $filePath;
+    }
+
+    public function log($message): void
+    {
+        file_put_contents($this->filePath, $message, FILE_APPEND);
+    }
+}
+
+$logger = new FileLogger('/var/log/app.log');
